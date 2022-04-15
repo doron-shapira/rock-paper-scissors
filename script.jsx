42b422c17dd1fb07fac5c1da567151f14a272ec8
Doron <doronking6400@gmail.com>
2022-04-15 23:49:41 +0300

    rock paper scissors buttons

--- script.jsx
+++ script.jsx
@@ -2,27 +2,60 @@ const root = document.getElementById('root');
 ReactDOM.render(<App />, root)
 
 function App() {
-    return <TitleAndScore />
+    return (
+        <>
+            <div className="title-and-score">
+                <Title />
+                <Score />
+            </div>
+            <div className="choices">
+                <Paper />
+                <Scissors />
+                <Rock />
+            </div>
+        </>
+    )
 }
 
 function Title() {
-    return <h1>ROCK PAPER SCISSORS</h1>
+    return <h1 className="title">ROCK PAPER SCISSORS</h1>
 }
 
 function Score() {
     return (
-        <div className='score-container'>
+        <div className="score-container">
             <div id="score-text">SCORE</div>
             <div id="score-number">0</div>
         </div>
     )
 }
 
-function TitleAndScore() {
+function Rock() {
     return (
-        <div className="title-and-score">
-            <Title />
-            <Score />
-        </div>
+        <button id="rock">
+            <div className="white-background">
+                <img src="images/icon-rock.svg" alt="rock" />
+            </div>
+        </button>
+    )
+}
+
+function Paper() {
+    return (
+        <button id="paper">
+            <div className="white-background">
+                <img src="images/icon-paper.svg" alt="paper" />
+            </div>
+        </button>
+    )
+}
+
+function Scissors() {
+    return (
+        <button id="scissors">
+            <div className="white-background">
+                <img src="images/icon-scissors.svg" alt="scissors" />
+            </div>
+        </button>
     )
 }
